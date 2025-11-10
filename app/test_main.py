4d727ba01709e178756dbf5161001589837a84f7
Anton Tsikhanovich <alluspeh@gmail.com>
2025-11-10 21:35:38 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,24 @@
-# write your code here
+from app.main import check_password
+import pytest
+
+@pytest.mark.parametrize(
+    "password, result",
+    [
+        pytest.param('Pass@word1', True,
+                     id="should return True if has_upper, has_digit, has_special"),
+        pytest.param('qwerty', False,
+                     id="returns False for passwords without uppercase letter"),
+        pytest.param('String', False,
+                     id="should  returns False for passwords without special symbols"),
+        pytest.param("", False,
+                     id="should return False if no password"),
+        pytest.param("Str@ng", False, id=" returns False for passwords without digits"),
+        pytest.param("Abcdwwefghijklmn1$A", False,
+                     id="should return False if password is longer than 16 characters"),
+        pytest.param("k123", False, id=" returns False for short passwords"),
+        pytest.param("qwerty%", False, id="returns False if forbidden symbol appear"),
+    ]
+)
+def test_check_password(password, result):
+    assert check_password(password) == result
+
